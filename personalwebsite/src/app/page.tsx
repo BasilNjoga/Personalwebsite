e57b5dc6896d6d8208b1bc3d2c10a72a62c8819c
Basil Njoga <Basilnjoga@gmail.com>
2023-08-12 22:40:54 +0300

    added webpage functionality and external links

--- personalwebsite/src/app/page.tsx
+++ personalwebsite/src/app/page.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <h1>Hello, I'm Basil</h1>
+  return (
+    <section>
+  <h1>Hello, I'm Basil</h1>
+  <br/>
+  <div>
+    I am a computer and software Engineer
+  </div>
+  I have a lot of experience in various capacities
+
+  Heres a list of some of my projects
+
+  <a href="/projects"> List </a>
+</section>
+  )
 }
---
+++ personalwebsite/src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <>
+    <div>
+      These are some of the projects i have undertaken
+    </div>
+    </>
+    )
+  }
